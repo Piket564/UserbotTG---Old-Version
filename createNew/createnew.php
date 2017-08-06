2a0b666d5458925a855f817bc75058d5406528e7
Piket564 <luca.iadicicco@vallauri.edu>
2017-08-06 13:01:39 +0200

    CreateNew

--- createNew/createnew.php
+++ createNew/createnew.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'vendor/autoload.php';
+require_once '../vendor/autoload.php';
 if (file_exists('.env')) {
     echo 'Loading .env...'.PHP_EOL;
     $dotenv = new Dotenv\Dotenv(getcwd());
