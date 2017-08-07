604da0735df357b15ee0c1ac09ab62d7535d9ce6
Piket564 <luca.iadicicco@vallauri.edu>
2017-08-07 12:01:02 +0200

    Bug Fix, add .gitHub command.
    Now the start.php provide a message with RCPE error and Exception to an
    array of Admin's id!
    :+1:

--- main.php
+++ main.php
@@ -16,3 +16,13 @@ if ($msg == '.status') {
     //reply
     sm($chatID,"I'm Workink!\nBot Online!",$msgID);
 }
+if ($msg == ".help") {
+    scrivendo($chatID);
+    $text="<a href='http://telegra.ph/Help-Page-08-06'>HELP!</a>\n\nDirectory test for main.php";
+    sm($chatID, $text, $msgID);
+}
+if($msg==".gitHub"){
+    scrivendo($chatID);
+    $text="https://github.com/Piket564/UserbotTG/";
+    sm($chatID, $text, $msgID);
+}
--- start.php
+++ start.php
@@ -1,5 +1,6 @@
 <?php
 require_once 'vendor/autoload.php';
+$admin=array(152500233);//admin ID's for error!
 if(!file_exists('session.madeline')){
     \danog\MadelineProto\Logger::log(["NO SESSION LOAD! PLEASE READ DOCS!"],\danog\MadelineProto\Logger::FATAL_ERROR);
     exit (0);
@@ -9,58 +10,78 @@ if(!file_exists('session.madeline')){
     function sm($ID, $text, $reply = false)
     {
         global $MadelineProto;
-        if($reply)
-        {
+        if ($reply) {
             $MadelineProto->messages->sendMessage(['peer' => $ID, 'message' => $text, 'parse_mode' => "HTML", 'reply_to_message_id' => $reply]);
         } else {
             $MadelineProto->messages->sendMessage(['peer' => $ID, 'message' => $text, 'parse_mode' => "HTML"]);
         }
     }
+
     /**/
-    function mindate($min){ return date('i', $min);}
-    function changeName($name){
+    function mindate($min)
+    {
+        return date('i', $min);
+    }
+
+    function changeName($name)
+    {
         global $MadelineProto;
         $MadelineProto->account->updateProfile(['first_name' => $name]);
     }
-    function changePropic($inputfile){
+
+    function changePropic($inputfile)
+    {
         global $MadelineProto;
         $inputFile = $MadelineProto->upload($inputfile);
         $MadelineProto->photos->uploadProfilePhoto(['file' => $inputFile]);
     }
-    function join_chat($chat){
+
+    function reply($chatID, $text, $tomsgId)
+    {
         global $MadelineProto;
-        $MadelineProto->messages->importChatInvite(['hash' => $chat ]);
+        $MadelineProto->messages->sendMessage(['peer' => $chatID, 'message' => $text, 'reply_to_msg_id' => $tomsgId]);
     }
-    function pinMex($msgID,$channelID,$silent=false){
+
+    function join_chat($chat)
+    {
         global $MadelineProto;
-        $MadelineProto->channels->updatePinnedMessage(['silent' => $silent, 'channel' => $channelID, 'id' => $msgID, ]);
+        $MadelineProto->messages->importChatInvite(['hash' => $chat]);
+    }
+
+    function pinMex($msgID, $channelID, $silent = false)
+    {
+        global $MadelineProto;
+        $MadelineProto->channels->updatePinnedMessage(['silent' => $silent, 'channel' => $channelID, 'id' => $msgID,]);
 
     }
-    function changeUsername($username){
+
+    function changeUsername($username)
+    {
         global $MadelineProto;
-        $MadelineProto->account->updateUsername(['username' => $username, ]);
+        $MadelineProto->account->updateUsername(['username' => $username,]);
     }
-    function leggimsg($chatID, $msgid) {
+
+    function readmsg($chatID, $msgid)
+    {
         global $update;
         global $MadelineProto;
         if (isset($chatID) and isset($msgid)) var_export($MadelineProto->messages->readHistory(['peer' => $chatID, 'max_id' => $msgid]));
     }
-    function isWriting($chatID) {
+
+    function scrivendo($chatID)
+    {
         global $update;
         global $MadelineProto;
         if (isset($chatID)) {
-            $sendMessageTypingAction = ['_' => 'sendMessageTypingAction', ];
+            $sendMessageTypingAction = ['_' => 'sendMessageTypingAction',];
             var_export($MadelineProto->messages->setTyping(['peer' => $chatID, 'action' => $sendMessageTypingAction]));
         }
     }
-
     $offset = 0;
     while (true) {
         $updates = $MadelineProto->API->get_updates(['offset' => $offset, 'limit' => 100, 'timeout' => 0]);
         foreach ($updates as $update) {
-            $offset = $update['update_id']+1;
-            //file_put_contents("logs/logs1.log",var_dump($update));
-            //file_put_contents("logs/logs2.log",var_dump($updates));
+            $offset = $update['update_id'] + 1;
             try {
                 $res = json_encode($update, JSON_PRETTY_PRINT);
                 if ($res == '') {
@@ -69,13 +90,19 @@ if(!file_exists('session.madeline')){
                 include("config.php");
             } catch (\danog\MadelineProto\RPCErrorException $e) {
                 \danog\MadelineProto\Logger::log(["Si e' verificato un errore di tipo RPCException: ".$e->getMessage()], \danog\MadelineProto\Logger::NOTICE);
+                foreach ($admin as $ad){
+                    sm($ad,"RPCE Exception:\n".$e->getMessage());
+                }
                 file_put_contents('logs/RPCException.log',$e->getMessage());
             } catch (\danog\MadelineProto\Exception $e) {
+                foreach ($admin as $ad){
+                    sm($ad,"Exception:\n".$e->getMessage());
+                }
                 \danog\MadelineProto\Logger::log(["Si e' verificato un errore di tipo Exception: ".$e->getMessage()], \danog\MadelineProto\Logger::NOTICE);
                 file_put_contents('logs/Exception.log',$e->getMessage());
             }
+            echo 'Wrote ' . \danog\MadelineProto\Serialization::serialize('session.madeline', $MadelineProto) . ' bytes' . PHP_EOL;
         }
-        echo 'Wrote ' . \danog\MadelineProto\Serialization::serialize('session.madeline', $MadelineProto) . ' bytes' . PHP_EOL;
     }
 }
 
